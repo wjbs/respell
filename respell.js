96e0d9d0b14c475c057362fe4760d507b7fd6448
wjbs <williambrownspencer@gmail.com>
2020-10-27 13:13:13 -0700

    Update respell.js

--- respell.js
+++ respell.js
@@ -3,7 +3,7 @@ $().ready(function() {alert("ready in respell.js");});
 $().ready(function() {TextToIPA.loadDict('./ipadict.txt');});
 
 $().ready(function(){
-        $.getJSON( "/foenemes.json", function( data ) {
+        $.getJSON("./foenemes.json", function( data ) {
         Window.phonemes = data[phonemes];
         Window.phonemeDict = data[phonemeDict];
         $("#out").html("test");
@@ -14,15 +14,39 @@ function englishToIpa(english) {
     return TextToIPA.lookup(englishTextArray[i].toLowerCase().replace(/[^\w\s]|_/g, '').replace(/\s+/g, ' '));
 }
 
+function setRespell(respelling) {
+    $("out").text(respelling);
+}
+
+function pickSpelling(phn) {
+    var spellings = Window.phonemeDict[Window.phonemes[phn]];
+    spellings = spellings.filter(s => s[0].search(".") === -1);
+    var spelling = spellings[Math.floor(Math.random() * spellings.length)];
+    return spelling;
+}
+
+function ipaRespell(ipa) {
+    for (i = 0; i < Window.phonemes.length; i++) {
+        ipa = ipa.replace(Window.phonemes[i], "{i}");
+    }
+    var phns = ipa.match(/\d+(?=})/g);
+    phns.foreach(phn => pickSpelling(phn));
+    var respelling = phns.reduce((acc, curr) => acc + curr, "");
+    phns.foreach(phn => respelling = respelling + "\n" + phn[0] + " as in " + phn[1])
+    return respelling
+}
+
 function convertWord() {
     alert("convertWord");
     var word = $("word-in").text();
-    alert(word);
+    var respelling = ipaRespell(englishToIpa(word));
+    setRespell(respelling);
 }
 
 
 function convertIpa() {
     alert("convertIpa");
     var ipa = $("word-in").text();
-    alert(ipa);
+    var respelling = ipaRespell(ipa);
+    setRespell(respelling);
 }
