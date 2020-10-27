7f7ac1459c4151402401b91d9b2ec4f5f913be22
wjbs <williambrownspencer@gmail.com>
2020-10-27 11:58:37 -0700

    Update respell.js

--- respell.js
+++ respell.js
@@ -3,18 +3,26 @@ $().ready(function() {alert("ready in respell.js");});
 $().ready(function() {TextToIPA.loadDict('./ipadict.txt');});
 
 $().ready(function(){
-    $.getJSON( "/foenemes.json", function( data ) {
-    Window.phonemes = data[phonemes];
-    Window.phonemeDict = data[phonemeDict];
-    $("#out").html("test");
-  });
+        $.getJSON( "/foenemes.json", function( data ) {
+        Window.phonemes = data[phonemes];
+        Window.phonemeDict = data[phonemeDict];
+        $("#out").html("test");
+    });
 });
+
+function englishToIpa(english) {
+    return TextToIPA.lookup(englishTextArray[i].toLowerCase().replace(/[^\w\s]|_/g, '').replace(/\s+/g, ' '));
+}
 
 funciton convertWord() {
     alert("convertWord");
+    var word = $("word-in").text();
+    alert(word);
 }
 
 
 funciton convertIpa() {
-    alert("convertWord");
+    alert("convertIpa");
+    var ipa = $("word-in").text();
+    alert(ipa);
 }
