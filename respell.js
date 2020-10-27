05fc41a2410619e4357edf581a5d4459fd992d1d
wjbs <williambrownspencer@gmail.com>
2020-10-27 11:45:44 -0700

    Update respell.js

--- respell.js
+++ respell.js
@@ -1,3 +1,11 @@
 $().ready(function() {alert("ready in respell.js");});
 
 $().ready(function() {TextToIPA.loadDict('./ipadict.txt');});
+
+$().ready(function(){
+    $.getJSON( "/foenemes.json", function( data ) {
+    Window.phonemes = data[phonemes];
+    Window.phonemeDict = data[phonemeDict];
+    $("#text").html(data["text"]);
+  });
+});
