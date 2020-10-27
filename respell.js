88fc7c3cfff9c740b49b60af5c782f27267980fc
wjbs <williambrownspencer@gmail.com>
2020-10-27 14:02:15 -0700

    Update respell.js

--- respell.js
+++ respell.js
@@ -40,9 +40,9 @@ function ipaRespell(ipa) {
         ipa = ipa.replace(p, "{"+ i.toString() + "}");
     }
     var phns = ipa.match(/\d+(?=})/g);
-    phns.foreach(phn => pickSpelling(phn));
+    phns.forEach(phn => pickSpelling(phn));
     var respelling = phns.reduce((acc, curr) => acc + curr, "");
-    phns.foreach(phn => respelling = respelling + "\n" + phn[0] + " as in " + phn[1])
+    phns.forEach(phn => respelling = respelling + "\n" + phn[0] + " as in " + phn[1])
     return respelling
 }
 
